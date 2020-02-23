d59650082a51e2023904387caee3e91f26b819b9
Willian Carvalho <Willian_matheus14@hotmail.com>
2020-02-23 19:56:17 -0300

    starting to implement the board

---
+++ ChessGame/Board/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Board
+{
+    class Board
+    {
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+        private Piece[,] Pieces;
+
+        public Board(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Pieces = new Piece[rows, columns];
+        }
+    }
+}
---
+++ ChessGame/Board/Piece.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; set; }
+        public int QtMovement { get; set; }
+        public Board Board { get; set; }
+
+        public Piece(Position position, Board board, Color color)
+        {
+            Position = position;
+            Board = board;
+            Color = color;
+            QtMovement = 0;
+        }
+
+
+
+    }
+}
