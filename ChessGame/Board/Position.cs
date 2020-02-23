7434183ab214c394195e20a436df5f8391b816f3
Willian Carvalho <Willian_matheus14@hotmail.com>
2020-02-23 19:35:42 -0300

    Initial commit

---
+++ ChessGame/Board/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Board
+{
+    class Position
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+
+        public Position(int column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public override string ToString()
+        {
+            return $"{Row}, {Column}";
+        }
+    }
+}
