ce0e0227765f36aa4c7f7f83a96366be3abd9871
Willian Carvalho <Willian_matheus14@hotmail.com>
2020-02-24 21:11:48 -0300

    Treating Exception errors with personalizated Exception

--- ChessGame/BoardData/Board.cs
+++ ChessGame/BoardData/Board.cs
@@ -22,10 +22,42 @@ namespace ChessGame.BoardData
             return Pieces[row, column];
         }
 
+        public Piece Piece(Position pos)
+        {
+            return Pieces[pos.Row, pos.Column];
+        }
+
+        public bool ExistPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return Piece(pos) != null;
+        }
+
         public void PutPiece(Piece p, Position pos)
         {
-            Pieces[pos.Row, pos.Column] = p;
+            if (ExistPiece(pos))
+            {
+                throw new BoardException("You already have a Piece in this position");
+            }
+            Pieces[pos.Column, pos.Row] = p;
             p.Position = pos;
+        }
+
+        public bool ValidPosition (Position pos)
+        {
+            if(pos.Row < 0 || pos.Row >= Rows || pos.Column < 0 || pos.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Invalid Position!");
+            }
         }
     }
 }
