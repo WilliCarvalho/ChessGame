a69279b91cec48f14bb5d9c73145ba152e0c7e71
Willian Carvalho <Willian_matheus14@hotmail.com>
2020-03-11 21:15:33 -0300

    Starting to implement other pieces

---
+++ ChessGame/ChessData/Bishop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChessGame.BoardData;
+
+namespace ChessGame.ChessData
+{
+    class Bishop : Piece
+    {
+        public Bishop(Board board, Color color) : base(board, color)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool CanMove(Position pos)
+        {
+            Piece p = Board.Piece(pos);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            //Verify Upper-Left
+            pos.SetValues(Position.Row - 1, Position.Column - 1);
+            while(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if(Board.Piece(pos) != null && Board.Piece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.SetValues(pos.Row - 1, pos.Column - 1);
+            }
+
+            return mat;
+        }
+    }
+}
